d69623645765e7e368537901aee92d33733a01a8
damian-wawrzyniak <damianwawrzyniak1@gmail.com>
2017-05-09 00:24:17 +0200

    Busket in progres

--- OnlineShop/OnlineShop.js
+++ OnlineShop/OnlineShop.js
@@ -35,15 +35,30 @@ $(document).ready(function (e) {
         prize: '30 pln',
         imageSource: 'http://www.popkiller.pl/sites/default/files/images/lona_i_webber.jpg'
     }];
-
+    var busket = [];
+    var moneyToPay = 0;
     function addItems() {
         for (i = 0; i < items.length; i++) {
             var img = "<img src=" + items[i].imageSource + " class=\"itemimage col-md-3\" />";
             var name = "<p class=\"itemname\">" + items[i].name + "</p>";
             var prize = "<p class=\"itemprize\">" + items[i].prize + "</p>";
-            var button = "<button class=\"itembutton\">Dodaj do koszyka</button>";
+            var button = "<button class=\"itembutton\" id=\""+i+"\">Dodaj do koszyka</button>";
             $("#shopcontent").append("<div class=\"item col-md-3\">" + img + name + prize + button + "</div>");
         }
     };
+    function updateBasket() {
+        var stuff = busket.pop;
+        var name = "<p class=\"itemname\">" + stuff.name + "</p>";
+        var closeIcon = "<span class=\"closeicon\">&#10006;</span>";
+        $("#shopbusket").append(name + closeIcon);
+        $("#moneytopay").text("Money to pay: " + moneyToPay);
+    };
+    function addToBusket(index) {
+        busket.push(items[index]);
+        var temp = items[index].prize.split(" ");
+        moneyToPay += parseInt(temp[0]);
+        updateBusket();
+    };
     addItems();
+    $("#0").click(function () { addToBusket(0); });
 });
