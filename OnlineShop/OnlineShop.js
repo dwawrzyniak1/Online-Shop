1c5bf05f08a273e664d6e7721663187414189b60
damian-wawrzyniak <damianwawrzyniak1@gmail.com>
2017-05-08 23:18:10 +0200

    Updating project before adding busket option

--- OnlineShop/OnlineShop.js
+++ OnlineShop/OnlineShop.js
@@ -16,6 +16,24 @@ $(document).ready(function (e) {
         name: 'Nirvana - Nevermind',
         prize: '20 pln',
         imageSource: 'https://upload.wikimedia.org/wikipedia/en/b/b7/NirvanaNevermindalbumcover.jpg'
+    },
+    {
+        id: '4',
+        name: 'Coldplay - Parachudes',
+        prize: '25 pln',
+        imageSource: 'https://upload.wikimedia.org/wikipedia/en/5/57/Coldplayparachutesalbumcover.jpg'
+    },
+    {
+        id: '5',
+        name: 'Goran Bregovic i Kayah - Kayah i Bregovic',
+        prize: '40 pln',
+        imageSource: 'http://i.iplsc.com/0003WY1CE6OXMAMB-C430.jpg'
+    },
+    {
+        id: '6',
+        name: 'Lona i Webber - Nawiasem mowiac',
+        prize: '30 pln',
+        imageSource: 'http://www.popkiller.pl/sites/default/files/images/lona_i_webber.jpg'
     }];
 
     function addItems() {
@@ -23,7 +41,8 @@ $(document).ready(function (e) {
             var img = "<img src=" + items[i].imageSource + " class=\"itemimage col-md-3\" />";
             var name = "<p class=\"itemname\">" + items[i].name + "</p>";
             var prize = "<p class=\"itemprize\">" + items[i].prize + "</p>";
-            $("#shopcontent").append("<div class=\"item col-md-3\">" + img + name + prize + "</div>");
+            var button = "<button class=\"itembutton\">Dodaj do koszyka</button>";
+            $("#shopcontent").append("<div class=\"item col-md-3\">" + img + name + prize + button + "</div>");
         }
     };
     addItems();
